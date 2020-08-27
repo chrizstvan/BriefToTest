cf479f9128ce623d70694148f5800c6d5e9029ad
chrizstvan <chris.stev30@gmail.com>
2020-08-27 20:55:12 +0700

    Initial Commit

---
+++ BriefToTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BriefToTest
+//
+//  Created by My Mac on 27/08/20.
+//  Copyright © 2020 Christian Stevanus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
